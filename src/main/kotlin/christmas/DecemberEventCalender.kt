08fce136412ecfacb06365f2e2d326fb498ee8e3
book3039 <book3039@knou.ac.kr>
2023-11-11 23:44:41 +0900

    refactor: DecemberEventCalender 가독성을 위해 리팩토링 및 함수 수정

--- src/main/kotlin/christmas/DecemberEventCalender.kt
+++ src/main/kotlin/christmas/DecemberEventCalender.kt
@@ -9,16 +9,15 @@ class DecemberEventCalender(private val day: Int = 1) : EventCalender(2023, 12, 
     }
 
     private fun findStarDays(): List<Int> =
-        findDateByDayName("SUNDAY") + listOf(25)
+        super.findDateByDayName("SUNDAY") + listOf(25)
 
     fun hasStar(): Boolean = starDays.contains(day)
 
     fun createDecemberEvents(): List<WoowaEvent> {
-        val events = mutableListOf<WoowaEvent>()
-        events.add(createDayTypeEvent())
+        val events = mutableListOf(super.createDayTypeEvent())
         if (hasStar()) events.add(SpecialEvent())
         if (day in 1..25) events.add(ChristmasDdayEvent())
 
-        return events
+        return events.toList()
     }
 }
