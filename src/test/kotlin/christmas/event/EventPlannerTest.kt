1a350a1f4a213a202abee2f0a4357f2ae90fe2fd
book3039 <book3039@knou.ac.kr>
2023-11-13 00:25:54 +0900

    test: 입력 받은 메뉴의 할인 전 총주문 금액을 구한다 테스트 작성

--- src/test/kotlin/christmas/event/EventPlannerTest.kt
+++ src/test/kotlin/christmas/event/EventPlannerTest.kt
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test
 
 class EventPlannerTest {
 
+    val eventPlanner = EventPlanner()
+
     @Test
     fun `입력 받은 메뉴를 (메뉴이름, 갯수) 형태로 바꾼다`() {
-        val eventPlanner = EventPlanner()
         val orders = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-3"
         assertThat(eventPlanner.parseMenu(orders)).isEqualTo(
             listOf(
@@ -17,6 +18,17 @@ class EventPlannerTest {
                 Pair("제로콜라", 3),
             )
         )
+    }
 
+    @Test
+    fun `입력 받은 메뉴의 할인 전 총주문 금액을 구한다_1`() {
+        val orders = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
+        assertThat(eventPlanner.calculateTotalOrderPrice(orders)).isEqualTo(142_000)
+    }
+
+    @Test
+    fun `입력 받은 메뉴의 할인 전 총주문 금액을 구한다_2`() {
+        val orders = "타파스-1,제로콜라-1"
+        assertThat(eventPlanner.calculateTotalOrderPrice(orders)).isEqualTo(8_500)
     }
 }
