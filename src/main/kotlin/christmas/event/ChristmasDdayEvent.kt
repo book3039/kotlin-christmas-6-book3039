3ac020157efa717078e1cb4a0e7a47dc02fc5609
book3039 <book3039@knou.ac.kr>
2023-11-13 03:53:13 +0900

    feat: ChristmasDdayEvent 클래스 작성

--- src/main/kotlin/christmas/event/ChristmasDdayEvent.kt
+++ src/main/kotlin/christmas/event/ChristmasDdayEvent.kt
@@ -1,5 +1,8 @@
 package christmas.event
 
-class ChristmasDdayEvent : WoowaEvent {
+class ChristmasDdayEvent(date: Int) : WoowaEvent {
 
+    override val benefit: Int = -1000 - (date - 1) * 100
+    override fun execute(eventPlanner: EventPlanner) {
+    }
 }
