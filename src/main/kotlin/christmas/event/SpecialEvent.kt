5cd9c3721b6974283a25ad1e034375f31a967885
book3039 <book3039@knou.ac.kr>
2023-11-13 03:52:32 +0900

    feat: SpecialEvent 클래스 작성

--- src/main/kotlin/christmas/event/SpecialEvent.kt
+++ src/main/kotlin/christmas/event/SpecialEvent.kt
@@ -1,5 +1,9 @@
 package christmas.event
 
-class SpecialEvent : WoowaEvent {
+class SpecialEvent() : WoowaEvent {
 
+    override val benefit: Int = -1000
+    override fun execute(eventPlanner: EventPlanner) {
+
+    }
 }
