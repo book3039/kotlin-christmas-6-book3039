bf6dce44498b3e4795c57db1aad7aec87621c0bb
book3039 <book3039@knou.ac.kr>
2023-11-13 03:53:43 +0900

    feat: WeekdayEvent 클래스 작성

--- src/main/kotlin/christmas/event/WeekDayEvent.kt
+++ src/main/kotlin/christmas/event/WeekDayEvent.kt
@@ -1,5 +1,17 @@
 package christmas.event
 
-class WeekDayEvent : WoowaEvent {
+import christmas.menu.DecemberMenu
 
+class WeekDayEvent() : WoowaEvent {
+
+    override var benefit: Int = 0
+    override fun execute(eventPlanner: EventPlanner) {
+        val dessertOrders = eventPlanner.parsedOrders
+            .filter { DecemberMenu().findCategory(it.first) == "디저트" }
+
+        for (order in dessertOrders) {
+            benefit += order.second * -2023
+
+        }
+    }
 }
