eaac38caf7e94079735aae87b6104feb1d8dad9d
book3039 <book3039@knou.ac.kr>
2023-11-13 03:55:58 +0900

    feat: EventPlanner init 함수 작성

--- src/main/kotlin/christmas/event/EventPlanner.kt
+++ src/main/kotlin/christmas/event/EventPlanner.kt
@@ -1,8 +1,21 @@
 package christmas.event
 
+import christmas.calender.DecemberEventCalender
 import christmas.menu.DecemberMenu
 
 class EventPlanner {
+
+    private val events: List<WoowaEvent>
+    private val orders = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
+    var parsedOrders = parseMenu(orders)
+
+
+    init {
+        val tempEvents = DecemberEventCalender(3).createDecemberEvents().toMutableList()
+        if (calculateTotalOrderPrice(orders) >= 120_000) tempEvents += listOf(GiveawayEvent())
+        events = tempEvents.toList()
+    }
+
     fun parseMenu(orders: String): List<Pair<String, Int>> {
         return orders.split(",").map {
             val (name, count) = it.split("-")
