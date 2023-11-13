3c29cce5517b0209c9387c0d54295895f57a0e14
book3039 <book3039@knou.ac.kr>
2023-11-13 11:05:18 +0900

    feat: 총 주문 금액이 10,000원 이상일때만 이벤트 적용 기능 구현

--- src/main/kotlin/christmas/planner/EventPlanner.kt
+++ src/main/kotlin/christmas/planner/EventPlanner.kt
@@ -7,15 +7,20 @@ import christmas.menu.DecemberMenu
 
 class EventPlanner {
 
-    private val events: List<WoowaEvent>
-    private val orders = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"
+    private val events: List<WoowaEvent> = listOf()
+    private val orders = "아이스크림-1"
     var parsedOrders = parseMenu(orders)
 
 
     init {
+        if (calculateTotalOrderPrice(orders) >= 10_000) setEvents()
+    }
+
+    private fun setEvents() {
         val tempEvents = DecemberEventCalender(3).createDecemberEvents().toMutableList()
         if (calculateTotalOrderPrice(orders) >= 120_000) tempEvents += listOf(GiveawayEvent())
-        events = tempEvents.toList()
+        events.toMutableList() += tempEvents
+        events.toList()
     }
 
     fun parseMenu(orders: String): List<Pair<String, Int>> {
