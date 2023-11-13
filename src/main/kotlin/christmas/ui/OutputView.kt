a31e9abc7330962b9aa0256e93ecd27ad446e902
book3039 <book3039@knou.ac.kr>
2023-11-13 23:05:59 +0900

    feat: 주문 메뉴 출력 기능 구현

--- src/main/kotlin/christmas/ui/OutputView.kt
+++ src/main/kotlin/christmas/ui/OutputView.kt
@@ -4,4 +4,10 @@ object OutputView {
     fun printEventPreview(date: Int) =
         println("12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
 
+    fun printMenu(parsedOrders: List<Pair<String, Int>>) {
+        println("<주문 메뉴>")
+        for (order in parsedOrders) {
+            println(order.first + " ${order.second}개")
+        }
+    }
 }
